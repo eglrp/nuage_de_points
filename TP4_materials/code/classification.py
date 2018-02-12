2b329760f7b490efac36e819760db0cd4e587da4
Leman FENG <flm8620@gmail.com>
2018-02-12 02:49:59 +0100

    TP4 prediction

--- TP4_materials/code/classification.py
+++ TP4_materials/code/classification.py
@@ -31,12 +31,16 @@ from sklearn.metrics import classification_report
 
 # Import functions to read and write ply files
 from utils.ply import write_ply, read_ply
-from descriptors import local_PCA, compute_features
+from descriptors import local_PCA, compute_features, compute_features_advanced
 
 # Import time package
 import time
 
 from os.path import exists
+
+#Keras
+import keras.utils.np_utils as np_utils
+import matplotlib.pyplot as plt
 
 
 # ------------------------------------------------------------------------------------------
@@ -178,6 +182,179 @@ def basic_training_and_test():
     t1 = time.time()
     print('Done in %.3fs\n' % (t1 - t0))
 
+    print('Save predictions ply')
+    t0 = time.time()
+    write_ply('../Lille_street_test_preds.ply',
+              [points, predictions],
+              ['x', 'y', 'z', 'labels'])
+    t1 = time.time()
+    print('Done in %.3fs\n' % (t1 - t0))
+
+def advanced_point_choice(num_per_class, radius, label_names):
+
+    # Initiate arrays
+    training_features = np.empty((0, 28))
+    training_labels = np.empty((0,))
+
+    # Loop over each training cloud
+    for i in range(1, 4):
+
+        # Load Training cloud
+        cloud_path = '../data/Lille_street_{:d}.ply'.format(i)
+        cloud_ply = read_ply(cloud_path)
+        points = np.vstack((cloud_ply['x'], cloud_ply['y'], cloud_ply['z'])).T
+        labels = cloud_ply['labels']
+
+        # Initiate training indices array
+        training_inds = np.empty(0, dtype=np.int32)
+
+        # Loop over each class to choose training points
+        for label, name in label_names.items():
+
+            # Do not include class 0 in training
+            if label == 0:
+                continue
+
+            # Collect all indices of the current class
+            label_inds = np.where(labels == label)[0]
+
+            # If you have not enough indices, just take all of them
+            if len(label_inds) <= num_per_class:
+                training_inds = np.hstack((training_inds, label_inds))
+
+            # If you have more than enough indices, choose randomly
+            else:
+                random_choice = np.random.choice(len(label_inds), num_per_class , replace=False)
+                training_inds = np.hstack((training_inds, label_inds[random_choice]))
+
+        # Compute features for the points of the chosen indices
+        features = compute_features_advanced(points[training_inds, :], points, radius)
+
+        # Concatenate features / labels of all clouds
+        training_features = np.vstack((training_features, features))
+        training_labels = np.hstack((training_labels, labels[training_inds]))
+
+    return training_features, training_labels
+
+import tensorflow as tf
+from keras.models import Sequential
+from keras.layers import Dense, Activation, Conv2D, MaxPooling2D, Flatten
+from keras.optimizers import SGD
+from keras.optimizers import Adam
+
+def deep_training_and_test(useGPU=True):
+
+    # Parameters
+    # **********
+    #
+
+    radius = 0.5
+    num_per_class = 16384*2
+    label_names = {0: 'Unclassified',
+                   1: 'Ground',
+                   2: 'Building',
+                   3: 'Vegetation',
+                   4: 'Barriers',
+                   5: 'Cars',
+                   6: 'Signage'}
+
+    # Collect training features / labels
+    # **********************************
+    #
+
+    print('Collect Training Features')
+    t0 = time.time()
+    training_features, training_labels = advanced_point_choice(num_per_class, radius, label_names)
+    training_labels -= 1
+    training_labels_one_hot = np_utils.to_categorical(training_labels, 6)
+    t1 = time.time()
+    print('Done in %.3fs\n' % (t1 - t0))
+
+
+    # Load cloud as a [N x 3] matrix
+    cloud_path = '../data/Lille_street_test.ply'
+    cloud_ply = read_ply(cloud_path)
+    points = np.vstack((cloud_ply['x'], cloud_ply['y'], cloud_ply['z'])).T
+    # Compute features only one time and save them for further use
+    #
+    #   WARNING : This will save you some time but do not forget to delete your features file if you change
+    #             your features. Otherwise you will not compute them and use the previous ones
+    #
+    feature_file = '../data/Lille_street_test_features_advanced.npy'
+    if exists(feature_file):
+        features = np.load(feature_file)
+    else:
+        print('compute feature for test data...')
+        t0 = time.time()
+        features = compute_features_advanced(points, points, radius)
+        np.save(feature_file, features)
+        t1 = time.time()
+        print('Done in %.3fs\n' % (t1 - t0))
+
+    # Train a MLP
+    # ********************************
+    #
+    device = '/device:GPU:0' if useGPU else '/device:CPU:0'
+    with tf.device(device):
+        model = Sequential()
+        model.add(Dense(200, activation='relu', input_shape=(28,)))
+        model.add(Dense(80, activation='relu'))
+        model.add(Dense(40, activation='relu'))
+        model.add(Dense(6, activation='softmax'))
+        model.compile(
+            loss='categorical_crossentropy',
+            optimizer='adam',
+            metrics=['accuracy']
+        )
+
+
+
+
+    print('Training MLP')
+    t0 = time.time()
+    history = model.fit(training_features, training_labels_one_hot,
+                        epochs=150, batch_size=2048, verbose=1, validation_split=0.1)
+    t1 = time.time()
+    print('Done in %.3fs\n' % (t1 - t0))
+
+    # Test
+    # ****
+    #
+
+    print('Compute testing features')
+    t0 = time.time()
+
+
+
+
+
+    t1 = time.time()
+    print('Done in %.3fs\n' % (t1 - t0))
+
+    print('Test')
+    t0 = time.time()
+    y_prob = model.predict(features)
+    predictions = y_prob.argmax(axis=-1)
+    predictions += 1
+    predictions = predictions.astype(float)
+    t1 = time.time()
+    print('Done in %.3fs\n' % (t1 - t0))
+
+    print('Save predictions')
+    t0 = time.time()
+    np.savetxt('../data/Lille_street_test_preds.txt', predictions, fmt='%d')
+    t1 = time.time()
+    print('Done in %.3fs\n' % (t1 - t0))
+
+    print('Save predictions ply')
+    t0 = time.time()
+    write_ply('../Lille_street_test_preds.ply',
+              [points, predictions],
+              ['x', 'y', 'z', 'labels'])
+    t1 = time.time()
+    print('Done in %.3fs\n' % (t1 - t0))
+
+
 
 # ------------------------------------------------------------------------------------------
 #
@@ -190,5 +367,4 @@ def basic_training_and_test():
 
 
 if __name__ == '__main__':
-
-    basic_training_and_test()
+    deep_training_and_test()
--- TP4_materials/code/descriptors.py
+++ TP4_materials/code/descriptors.py
@@ -61,15 +61,22 @@ def local_PCA(points):
 def neighborhood_PCA(query_points, cloud_points, radius):
 
     # This function needs to compute PCA on the neighborhoods of all query_points in cloud_points
+    print("building kdtree..")
     tree = KDTree(cloud_points)
+    print("querying ..")
     nbs = tree.query_radius(query_points, radius)
+    print("calculating PCA ..")
 
     all_eigenvalues = np.zeros((query_points.shape[0], 3))
     all_eigenvectors = np.zeros((query_points.shape[0], 3, 3))
 
     for i in range(query_points.shape[0]):
         nb = nbs[i]
-        ev, evec = local_PCA(cloud_points[nb,:])
+        if len(nb) > 0:
+            ev, evec = local_PCA(cloud_points[nb,:])
+        else:
+            ev = np.array([1, 1, 1])
+            evec = np.eye(3)
         all_eigenvalues[i, :] = ev
         all_eigenvectors[i, :, :] = evec
 
@@ -79,7 +86,7 @@ def neighborhood_PCA(query_points, cloud_points, radius):
 def compute_features(query_points, cloud_points, radius):
 
     # Compute the features for all query points in the cloud
-    all_eigenvalues, all_eigenvectors = neighborhood_PCA(cloud, cloud, 0.5)
+    all_eigenvalues, all_eigenvectors = neighborhood_PCA(query_points, cloud_points, radius)
 
     l1 = all_eigenvalues[:, 2]
     l2 = all_eigenvalues[:, 1]
@@ -88,11 +95,46 @@ def compute_features(query_points, cloud_points, radius):
     normals = all_eigenvectors[:, :, 0]
     ez = np.array([0., 0., 1.])
     verticality = 2 * np.arcsin(np.abs(normals.dot(ez.T))) / np.pi
+    l1 += 1e-10
     linearity = 1 - l2/l1
     planarity = (l2-l3)/l1
     sphericity = l3/l1
 
     return verticality, linearity, planarity, sphericity
+
+def compute_features_with_eig_vec(query_points, cloud_points, radius):
+
+    # Compute the features for all query points in the cloud
+    all_eigenvalues, all_eigenvectors = neighborhood_PCA(query_points, cloud_points, radius)
+
+    l1 = all_eigenvalues[:, 2]
+    l2 = all_eigenvalues[:, 1]
+    l3 = all_eigenvalues[:, 0]
+
+    normals = all_eigenvectors[:, :, 0]
+    direction1 = all_eigenvectors[:, :, 2]
+    direction2 = all_eigenvectors[:, :, 1]
+    ez = np.array([0., 0., 1.])
+    verticality = 2 * np.arcsin(np.abs(normals.dot(ez.T))) / np.pi
+    l1 += 1e-10
+    linearity = 1 - l2/l1
+    planarity = (l2-l3)/l1
+    sphericity = l3/l1
+
+    return verticality, linearity, planarity, sphericity, normals, direction1, direction2
+
+def compute_features_advanced(query_points, cloud_points, radius):
+    verticality, linearity, planarity, sphericity, normals, direction1, direction2 = \
+        compute_features_with_eig_vec(query_points, cloud_points, radius)
+
+    features = np.vstack((verticality, linearity, planarity, sphericity))
+
+    for dir in [direction1, -direction1, direction2, -direction2, normals, -normals]:
+        verticality, linearity, planarity, sphericity = \
+            compute_features(query_points + dir*radius, cloud_points, radius)
+        features = np.vstack((features, verticality, linearity, planarity, sphericity))
+
+    return features.T
 
 
 # ------------------------------------------------------------------------------------------
